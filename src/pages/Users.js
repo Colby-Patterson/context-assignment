c5d2b257b656eed6f408a2ff14aded20545f0f9d
Colby Patterson <cpatt@byui.edu>
2022-06-02 15:21:49 -0600

    Building users component

---
+++ src/pages/Users.js
@@ -0,0 +1,38 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+function Users() {
+  let [users, setUsers] = useState([])
+  let [loading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    getUsers
+  }, [])
+
+  const renderUsers = ()=>{
+    return users.map((user)=>{
+      return (
+        <div key={user.id}>
+          <p>{user.id}</p>
+        </div>
+      )
+    })
+  }
+
+  const getUsers = async()=>{
+    setLoading(true)
+    let result = await axios.get("https://reqres.in/api/users?delay=3")
+    setLoading(false)
+    setUsers(result.data.data)
+  }
+
+
+  return (
+    <>
+    <p>Users</p>
+    {renderUsers()}
+    </>
+  )
+}
+
+export default Users
